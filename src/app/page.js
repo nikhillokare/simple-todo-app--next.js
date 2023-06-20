a602dc5e079f4a8b26d4b10645fa31d0bbbbd6ef
Nikhil <nikhillokare6@gmail.com>
2023-06-20 17:03:40 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,98 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editValue, setEditValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== "") {
+      if (editMode) {
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex] = newTodo;
+        setTodos(updatedTodos);
+        setEditIndex(null);
+        setEditValue("");
+        setEditMode(false);
+      } else {
+        setTodos([...todos, newTodo]);
+      }
+      setNewTodo("");
+    }
+  };
+
+  const handleTodoComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = `✅ ${updatedTodos[index]}`;
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoEdit = (index, value) => {
+    setEditMode(true);
+    setEditIndex(index);
+    setEditValue(value);
+    setNewTodo(value);
+  };
+
+  const cancelEditMode = () => {
+    setEditMode(false);
+    setEditIndex(null);
+    setEditValue("");
+    setNewTodo("");
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={styles.container}>
+      <h1>Todo App</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <form onSubmit={handleFormSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Enter a new todo"
+          value={newTodo}
+          onChange={handleInputChange}
         />
-      </div>
+        <button type="submit">{editMode ? "Update Todo" : "Add Todo"}</button>
+        {editMode && (
+          <button type="button" onClick={cancelEditMode}>
+            Cancel
+          </button>
+        )}
+      </form>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <ul className={styles.todos}>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span>{todo}</span>
+            {!editMode && (
+              <div>
+                <button onClick={() => handleTodoComplete(index)}>
+                  Complete
+                </button>
+                <button onClick={() => handleTodoEdit(index, todo)}>
+                  Edit
+                </button>
+                <button onClick={() => handleTodoDelete(index)}>Delete</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
